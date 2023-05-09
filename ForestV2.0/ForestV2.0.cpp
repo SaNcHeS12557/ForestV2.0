44be773e1b45970371e5cea34ff6a361d3149a48
Oleksandr Shvarchevskyi <so53937@zut.edu.pl>
2023-05-09 17:51:34 +0200

    drawHouse()

--- ForestV2.0/ForestV2.0.cpp
+++ ForestV2.0/ForestV2.0.cpp
@@ -10,30 +10,37 @@ using namespace std;
 #include "Border.h"
 #include "Trapeze.h"
 
-//int Tree::counter = 0; // liczba drzew
 int Shape::counter = 0; // liczba objektow
 
-int main() {
-    Forest las = Forest(30, 100);
-
+void drawHouse(int x, int y, Forest& las) {
     Rectangle* walls = new Rectangle(5, 14, '*', "green");
     Rectangle* door = new Rectangle(3, 3, '*', "yellow");
-    _Tree* leaves = new _Tree(5, '*', "green");
-    Rectangle* trunk = new Rectangle(1, 3, '#', "red");
     Trapeze* roof = new Trapeze(4, '*', "red");
     Border* window = new Border(3, 3, '#', "red", 1);
 
+    _Tree* leaves = new _Tree(5, '*', "green");
+    Rectangle* trunk = new Rectangle(1, 3, '#', "red");
+
+
+    roof->setXY(x, y);
+    las += roof;
+    walls->setXY(x+3, y+4);
+    las += walls;
+    door->setXY(x+5, y+6);
+    las += door;
+    window->setXY(x+10, y+5);
+    las += window;
+}
+
+int main() {
+
+    Forest las = Forest(30, 100);
+
     //las.AddShape(door, 4, 7);
 
-    roof->setXY(3, 1);
-    las += roof;
-    walls->setXY(6, 5);
-    las += walls;
-    door->setXY(8, 7);
-    las += door;
-    window->setXY(13, 6);
-    las += window;
-
+    drawHouse(10, 10, las);
+    drawHouse(1, 1, las);
+    drawHouse(10, 20, las);
 
     las.printTab();
 }
